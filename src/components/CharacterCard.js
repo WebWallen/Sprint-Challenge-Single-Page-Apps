2ed587604f64987253bd1077819e8583ffae04b0
Daniel Wallen <daniel.wallen1987@gmail.com>
2019-08-23 11:47:22 -0400

    Character Card Complete

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,32 @@
 import React from "react";
+import styled from "styled-components";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+const GridContainer = styled.div`
+    display: inline-grid;
+    grid-template-columns: auto auto auto;
+    background-color: transparent;
+    padding: 10px;
+`
+
+const GridItem = styled.div`
+  background-color: rgba(255, 255, 255, 0.8);
+  border: 1px solid rgba(0, 0, 0, 0.8);
+  padding: 20px;
+`
+
+const CharacterCard = props => {
+  return (
+    <GridContainer>
+      <GridItem>
+        <img src={props.result.image} />
+        <h2>{props.result.name}</h2>
+        <p>{props.result.status}: {props.result.species}</p>
+        <p>{props.result.location.name}</p>
+        {/* <p>{props.result.location}</p> */}
+        {/* <p>{props.result.origin}</p> */}
+      </GridItem>
+    </GridContainer>
+  )
 }
+
+export default CharacterCard;
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,33 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import CharacterCard from "./CharacterCard";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [charactersArray, setCharacters] = useState([]);
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+    axios
+      .get('https://rickandmortyapi.com/api/character/')
+      .then(response => {
+        console.log(response);
+        const characters = response.data.results;
+        setCharacters(characters);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+
+  }, [charactersArray]);
 
   return (
     <section className="character-list grid-view">
       <h2>TODO: `array.map()` over your state here!</h2>
+      {
+        charactersArray.map(character => <CharacterCard result = {character} />)
+      }
     </section>
   );
 }
